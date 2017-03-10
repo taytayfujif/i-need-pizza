d5d0e384f502cb552d962a7d35a9cb4a5087ec0d
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-03-10 13:52:29 -1000

    finished coding step 2

---
+++ pizza.js
@@ -0,0 +1,134 @@
+let order11821 = {
+  "Customer Name": "James Franko",
+  "Address": "2155 E Lake Dr",
+  "City": "Seattle",
+  "State": "WA",
+  "Zip Code": "11310",
+  "Pizza": {
+    "Crust": "thin",
+    "Pizza Sauce": "robust marinara",
+    "Cheese": "regular",
+    "Toppings": {
+      "Veggies": ["olives", "green peppers"],
+      "Meat": ["pepperoni", "ham", "sausage"]
+    }
+  },
+  "Side Items": ["breadsticks", "bbq wings 6 piece"],
+  "Sauces": "Ranch",
+  "Special Instructions": "Please cut pizza into squares",
+  "Payment Type": "cash",
+  "Store Id": 112
+}
+
+let order11372 = {
+  "Customer Name": "Justin Bieber",
+  "Address": "1801 W Rodeo Dr",
+  "City": "Beverly Hills",
+  "State": "CA",
+  "Zip Code": "91833",
+  "Pizza": {
+    "Crust": "stuffed crust",
+    "Pizza Sauce": "creamy garlic alfredo",
+    "Cheese": "double",
+    "Toppings": {
+      "Veggies": ["onion", "anchovies", "mushrooms"],
+      "Meat": ["chicken", "bacon"]
+    }
+  },
+  "Sides": "doube fudge brownie dessert",
+  "Sauces": ["bbq", "garlic sauce"],
+  "Special Instructions": "Please call 815 at the gate",
+  "Payment Type": "credit card",
+  "Store Id": 382
+}
+
+let order17844 = {
+  "Customer Name": "selena Gomez",
+  "Address": "9435 Haine St",
+  "City": "San Diego",
+  "State": "CA",
+  "Zip Code": "94333",
+  "Pizza": {
+    "Crust": "hand tossed",
+    "Pizza Sauce": "bbq",
+    "Cheese": "light",
+    "Toppings": {
+      "Veggies": ["pinapple", "jalapeno", "spinach"],
+      "Meat": "ham",
+    }
+  },
+  "Sides": "garlic knots",
+  "Sauces": "N/A",
+  "Special Instructions": "Please make crust extra crunchy",
+  "Payment Type": "member reward points",
+  "Store Id": 586
+}
+
+
+let currentOrders = {
+  "order11821": {
+    "Customer Name": "James Franko",
+    "Address": "2155 E Lake Dr",
+    "City": "Seattle",
+    "State": "WA",
+    "Zip Code": "11310",
+    "Pizza": {
+      "Crust": "thin",
+      "Pizza Sauce": "robust marinara",
+      "Cheese": "regular",
+      "Toppings": {
+        "Veggies": ["olives", "green peppers"],
+        "Meat": ["pepperoni", "ham", "sausage"]
+      }
+    },
+    "Side Items": ["breadsticks", "bbq wings 6 piece"],
+    "Sauces": "Ranch",
+    "Special Instructions": "Please cut pizza into squares",
+    "Payment Type": "cash",
+    "Store Id": 112
+  },
+  "order11372": {
+    "Customer Name": "Justin Bieber",
+    "Address": "1801 W Rodeo Dr",
+    "City": "Beverly Hills",
+    "State": "CA",
+    "Zip Code": "91833",
+    "Pizza": {
+      "Crust": "stuffed crust",
+      "Pizza Sauce": "creamy garlic alfredo",
+      "Cheese": "double",
+      "Toppings": {
+        "Veggies": ["onion", "anchovies", "mushrooms"],
+        "Meat": ["chicken", "bacon"]
+      }
+    },
+    "Sides": "doube fudge brownie dessert",
+    "Sauces": ["bbq", "garlic sauce"],
+    "Special Instructions": "Please call 815 at the gate",
+    "Payment Type": "credit card",
+    "Store Id": 382
+  },
+  "order17844": {
+    "Customer Name": "selena Gomez",
+    "Address": "9435 Haine St",
+    "City": "San Diego",
+    "State": "CA",
+    "Zip Code": "94333",
+    "Pizza": {
+      "Crust": "hand tossed",
+      "Pizza Sauce": "bbq",
+      "Cheese": "light",
+      "Toppings": {
+        "Veggies": ["pinapple", "jalapeno", "spinach"],
+        "Meat": "ham",
+      }
+    },
+    "Sides": "garlic knots",
+    "Sauces": "N/A",
+    "Special Instructions": "Please make crust extra crunchy",
+    "Payment Type": "member reward points",
+    "Store Id": 586
+  }
+}
+
+console.log('currentOrders: ', currentOrders);
