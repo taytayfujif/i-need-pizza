d1d1c9f9b9f204ee583d277f149ae1a352e96eb4
Tyler Nichols <tylernchls@gmail.com>
2017-03-11 11:45:35 -1000

    Update pizza.js

--- js/pizza.js
+++ js/pizza.js
@@ -1,7 +1,7 @@
 /*
 * Create a variable for each order with the name related to the order number
 * Create an object for each order based on the specs in the ReadMe
-* Each object will have two nested objects (pizza & toppings)
+* Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
 
