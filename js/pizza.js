e0445de4c721e69e0e94dc8f6dd950e77a580f84
taytayfujif <31020217+taytayfujif@users.noreply.github.com>
2018-02-07 14:35:29 -1000

    1st commit

--- js/pizza.js
+++ js/pizza.js
@@ -3,14 +3,55 @@
 * Create an object for each order based on the specs in the ReadMe
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
-*/
+*/ 
+var order11821= {
+	CustomerName: "James Franko",
+	Address: "2155 E Lake Dr"
+	City: "Seattle"
+	State: "WA"
+	ZipCode: 11310
+	Pizza:
+  		Crust: "thin"
+  		Pizza Sauce: "robust marinara"
+  		Cheese: "regular"
+  		Toppings:
+   			 Veggies: "olives, green peppers"
+    		 Meats: "pepperoni, ham, sausage"
+	SideItems: "breadsticks, bbq wings 6 piece"
+	Sauces: "ranch"
+	SpecialInstructions: "please cut into squares"
+	Payment "Type: cash"
+	Store Id: 112
+}
 
+var order11372= {
+	CustomerName: "Justin Bieber"
+	Address: "1801 W Rodeo Dr"
+	City: "Beverly Hills"
+	State: "CA"
+	ZipCode: 91833
+	Pizza:
+ 		 Crust: "stuffed crust"
+ 		 Pizza Sauce: "creamy garlic alfredo"
+ 		 Cheese: "double"
+ 		 Toppings:
+   			 Veggies: "onion, anchovies, mushrooms"
+   			 Meats: "chicken, bacon"
+	SideItems: "double fudge brownie dessert"
+	Sauces: "bbq, garlic sauce"
+	Special Instructions: "call 815 at the gate"
+	Payment Type: "credit card"
+	Store Id: 382
+}
 
 /*
 * Create a new variable named currentOrders
 * currentOrders should be single object containing both orders from above
 */
+ var current0rders={
+ 		
 
+ }
 /*
 * Make the changes to the currentOrders object based on the ReadMe
 * Run your file with node pizza.js
